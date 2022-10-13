10828e170bbe8739214bc9d2597bd37baa59c3c4
Gelfeto Gebre <gelf.learing@gmail.com>
2022-10-13 08:31:09 +0000

    this time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
 *main- prints a string
-*Return: nothing.
+*Return: nothing(0).
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
