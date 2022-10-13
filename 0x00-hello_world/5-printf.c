1e9c3ca23b131806dc36e3bf446ff5341f0b2e15
Gelfeto Gebre <gelf.learing@gmail.com>
2022-10-13 08:41:10 +0000

    thi is not a good idea

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main- print the string with proper grammer.
+*Return: always zero.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
