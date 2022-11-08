ddee8e0dbf55af3ff4b9c6bce7f4db6f5e5f3dd8
Gelfeto Gebre <gelfeto.gh@gmail.com>
2022-11-08 17:26:33 +0300

    correction

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,42 +1,45 @@
-
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 /**
- * alloc_grid - nested loop
- * @width: width
- * @height: height
- * Return: pointer to 2D array
+ * alloc_grid - Returns a pointer to a 2-dimensional
+ * @width: The width
+ * @height: The height
+ *
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the 2-dimensional array of integers.
  */
 int **alloc_grid(int width, int height)
 {
-	int **ptr;
-	int i, n;
+	int **twoD;
+	int hgt_index, wid_index;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * height);
+	twoD = malloc(sizeof(int *) * height);
 
-	if (ptr == NULL)
-	{
-		free(ptr);
+	if (twoD == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < height; i++)
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		ptr[i] = malloc(sizeof(int) * width);
-		if (ptr == NULL)
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+
+		if (twoD[hgt_index] == NULL)
 		{
-			free(ptr);
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
+
+			free(twoD);
 			return (NULL);
 		}
-
-		for (n = 0; n < width; n++)
-		{
-			ptr[i][n] = 0;
-		}
 	}
-	return (ptr);
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
+	}
+
+	return (twoD);
 }
