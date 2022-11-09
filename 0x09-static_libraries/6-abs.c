228786078cbc974db5ff9d56c7c3ad3d6cb3141f
Gelfato Gebre <gelf.learing@gmail.com>
2022-11-09 08:23:09 +0000

    stacked

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - find the abs of the int
+ * @i: i is an integer
+ *
+ * Return: integer
+ */
+
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	else if (i < 0)
+	{
+		return (-i);
+	}
+	else
+	{
+		return (i);
+	}
+}
