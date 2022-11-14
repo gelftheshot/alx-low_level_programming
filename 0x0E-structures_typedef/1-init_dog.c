169d83bbe6618c2f513156307c0975c2c76e2379
Gelfeto Gebre <gelf.learing@gmail.com>
2022-11-14 19:00:19 +0000

    hh

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 
 /**
 *init_dog - is func to init ele
-*@d - str of dog
+*@d: str of dog
 *@name: name
 *@age: age of the dog
 *@owner: the owner
@@ -14,10 +14,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
-	else
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
