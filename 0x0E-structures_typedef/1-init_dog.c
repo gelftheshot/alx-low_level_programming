856bc58e87896180ea61fe188f8f4a881c677599
Gelfeto Gebre <gelf.learing@gmail.com>
2022-11-14 18:51:39 +0000

    fixing some corrections

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <dog.h>
+#include "dog.h"
 #include <stdlib.h>
 
 /**
@@ -16,8 +16,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		return;
 	else
 	{
-		d->name = name
-		d->age = age
-		d->owner = owner
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
