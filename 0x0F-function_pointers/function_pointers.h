ef315ae4df17d9cc4af886d79f389ad55dfb0f90
Gelfeto Gebre <gelf.learing@gmail.com>
2022-11-17 18:17:54 +0000

    woring

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,4 +2,5 @@
 #define FUNCTION_POINTER_H
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
