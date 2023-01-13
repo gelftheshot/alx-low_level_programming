f2fe075a75df6fc5daff17af4d1577bdd870f750
Gelfeto <gelf.learing@gmail.com>
2023-01-13 16:08:55 +0300

    fixation

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,10 +12,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t  *temp;
 	dlistint_t *ptr1 = *h, *ptr2 = *h;
-
+	unsigned int i = 0;
 	temp = malloc(sizeof(dlistint_t));
 	if (temp == NULL)
 		return (NULL);
+	
+	while (ptr1)
+	{
+		i++;
+		ptr1 = ptr1->next;
+	}
+
+	if (idx > i)
+		return (NULL);
+	
+	ptr1 = *h;
+
 	while (idx--)
 	{
 		ptr2 = ptr1;
