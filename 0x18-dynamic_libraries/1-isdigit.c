bb4e39fb1b0d77b70f74af9ee1f58749f382155c
Gelfeto <gelf.learing@gmail.com>
2023-01-16 14:09:09 +0300

    static library

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _isdigit - chechk if the givern numbr is digit
+* @c: is comparing variable
+* Return: 1 if digit 0 else
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+
+}
+
