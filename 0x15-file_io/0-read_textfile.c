58a0df762c4b726ca9f387b3084619293f219995
Gelfato Gebre <gelf.learing@gmai.com>
2023-08-07 14:55:40 +0300

    fixing malloc errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,18 +15,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
-	op = open(filename, O_RDONLY);
-	if (op == -1)
-		return (0);
-
-	buff = malloc(letters * sizeof(char));
+	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
+	op = open(filename, O_RDONLY);
+	if (op == -1)
+	{
+		free(buff);
+		return (0);
+	}
 
 	re = read(op, buff, letters);
 	if (re == -1)
+	{
+		free(buff);
 		return (0);
+	}
 	wr = write(STDOUT_FILENO, buff, re);
 
 	if (wr == -1 || re != wr)
@@ -34,6 +38,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buff);
 		return (0);
 	}
+
 	free(buff);
 	return (wr);
 }
