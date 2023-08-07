97de72cba3d70c767d134cb1b19c1c70af67a9f8
Gelfato Gebre <gelf.learing@gmai.com>
2023-08-07 13:38:56 +0300

    fixing betty errors from my first file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,34 @@
 #include "main.h"
 /**
-*
-*
-*/
+ *read_textfile - read a text from fiela and print it
+ *@letters: is the number of letter to be printed
+ *@filename: is the file name
+ *Return: the number of letter printed
+ *	or 0 for failer case
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int op,re,wr;
+	int op, re, wr;
 
-    char *buff;
-    if (filename == NULL){
-        return (0);
-    }
+	char *buff;
 
-    op = open(filename,O_RDWR);
-    if(op == -1)
-        return (0);
-    buff = malloc(letters);
-    re = read(op, buff,letters);
-    wr = write(1, buff, re);
-        if(re == -1 || wr == -1 || re != wr){
-        free(buff);
-        return(0);
-    }
-    free(buff);
-    return (wr);
+	if (filename == NULL)
+		return (0);
 
+	op = open(filename, O_RDWR);
+	if (op == -1)
+		return (0);
+	buff = malloc(letters);
+
+	re = read(op, buff, letters);
+	wr = write(1, buff, re);
+
+	if (re == -1 || wr == -1 || re != wr)
+	{
+		free(buff);
+		return (0);
+	}
+	free(buff);
+	return (wr);
 }
