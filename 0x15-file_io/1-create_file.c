5b23dc8639bdbf99704e9ef2afb13e666e460d6f
Gelfato Gebre <gelf.learing@gmai.com>
2023-08-07 16:34:42 +0300

    fixing unknown error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,12 +8,12 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t op, wr;
+	int op, wr;
 	int j;
 
 	if (filename == NULL)
 		return (-1);
-	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 	if (op == -1)
 		return (-1);
 	if (text_content != NULL)
@@ -25,6 +25,7 @@ int create_file(const char *filename, char *text_content)
 	wr = write(op, text_content, j);
 	if (wr == -1)
 		return (-1);
+
 	close(op);
 	return (1);
 }
