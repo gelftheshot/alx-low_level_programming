088d3a921e40cd8138cdd67c0dca52c851e1050f
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-05 21:01:36 +0300

    some of the hashtable functoins

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_create - a functiong that creates a hash table.
+ *@size: is the size of the hash table
+ *Return: the created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_table_t *new_hash = malloc(sizeof(hash_table_t));
+
+	if (new_hash == NULL)
+		return (NULL);
+	new_hash->size = size;
+	new_hash->array = malloc(sizeof(char *) * size);
+	if (new_hash->array == NULL)
+	{
+		free(new_hash);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		new_hash->array[i] = NULL;
+		i++;
+	}
+	return (new_hash);
+}
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ *key_index - is a function that returns the value to key
+ *@key: the given key
+ *@size: the size of the hash table
+ *@Return: the value of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int value;
+	value = hash_djb2(key);
+	return (value % size);	
+}
