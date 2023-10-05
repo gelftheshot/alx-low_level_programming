429c070e55c394531c26cfb30181aa818866801b
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-05 21:08:11 +0300

    correcting betty coding style

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,11 +4,12 @@
  *key_index - is a function that returns the value to key
  *@key: the given key
  *@size: the size of the hash table
- *@Return: the value of the key
+ *Return: the value of the key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int value;
+
 	value = hash_djb2(key);
-	return (value % size);	
+	return (value % size);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,5 @@
 #ifndef _HASH_TABELS_
-#define _HASH_TABLES_
+#define _HASH_TABELS_
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -12,10 +12,11 @@
 
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
+
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -28,15 +29,18 @@ typedef struct hash_node_s
 
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
 #endif
