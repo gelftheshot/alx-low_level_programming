8d3abe25ea6505317a794bc7dfab676dfc9fb8f9
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-06 10:24:28 +0300

    betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,16 +1,16 @@
 #include "hash_tables.h"
 
 /**
- *
- *
- *
+ *hash_table_delete - free the hash table
+ *@ht: the hash table
+ *Return: Nothing
 **/
 
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i  = 0;
 	hash_node_t *n;
-	
+
 	while (i < ht->size)
 	{
 		n = ht->array[i];
