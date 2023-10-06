56279b2d58a0db85699bb87e49eccd394c0fa754
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-06 10:48:23 +0300

    fixing edge cases

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,22 +10,21 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-
-	index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *n;
 
 	if (key == NULL || ht == NULL || *key == '\0')
 	{
 		return (NULL);
 	}
 
-	if (index >= ht->size)
-		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
 
-	while (ht->array[index])
+	n = ht->array[index];
+	while (n)
 	{
-		if (strcmp(ht->array[index]->key, key) == 0)
-			return (ht->array[index]->value);
-		ht->array[index] = ht->array[index]->next;
+		if (strcmp(n->key, key) == 0)
+			return (n->value);
+		n = n->next;
 	}
 
 	return (NULL);
